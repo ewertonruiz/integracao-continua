65494d54529267ecbf75d0f43047fcf66a1da828
ewerton <ewertonruiz@gmail.com>
2022-11-23 15:07:00 -0500

    restaurando o front-end  e back-end do SGCM

--- back-end/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ back-end/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -19,7 +19,9 @@ public class FiltroCors {
         corsConfig.setAllowedOrigins(
             Arrays.asList(
                 "http://127.0.0.1:5500",
-                "https://localhost:4200")
+                "https://localhost:4200",
+                "https://sgcm-ewerton2-production.up.railway.app",
+                "https://sgcmapp.netlify.app")
         );
         corsConfig.setAllowedMethods(Arrays.asList("*"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
